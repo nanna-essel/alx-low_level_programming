4d0da9408b1babce04b06ea3bec78ddab079aa73
nanna-essel <hinannakojo@gmail.com>
2023-11-01 08:52:15 -0700

    uodated hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
-#include <stdio.h>                                                                                                                      
-/**                                                                                                                                     
- * main - Entry                                                                                                                         
- * Return: Always 0                                                                                                                     
- */                                                                                                                                     
-int main(void)                                                                                                                          
-{                                                                                                                                       
-        puts("\"Programming is like building a multi lingual puzzle\"");                                                            
-        return (0);
+#include <stdio.h>
+
+/**
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
