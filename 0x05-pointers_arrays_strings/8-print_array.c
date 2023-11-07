6cd361ae9c2796f170441256f0d97587804ba322
nanna-essel <hinannakojo@gmail.com>
2023-11-07 06:01:56 -0800

    print n element

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,4 +9,15 @@
  * Return: Nothing
  */
 
+void print_array(int *a, int n)
+{
+	int i = 0;
 
+	for (; i < n; i++)
+	{
+		printf("%d", *(a + i));
+		if (i != (n - 1))
+			printf(", ");
+	}
+	printf("\n");
+}
