d7bd05468b38a3a033d1af07b70ae2b7e8238246
nanna-essel <hinannakojo@gmail.com>
2023-11-10 10:40:03 -0800

    his function appends the src string to the dest string

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+i#include "main.h"
+
+/*This function appends the src string to the dest string,
+ * overwriting the terminating null byte (\0) at the end of dest
+ * and then adds a terminating null byte
+ * Returns a pointer to the resulting string dest
+ */
+
+char *_strcat(char *dest, char *src);
+
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+	}
+	for (j = 0; src[j] != '\0'; j++, i++)
+	{
+		dest[i] = src[j];
+	}
+
+	return (dest);
+}
